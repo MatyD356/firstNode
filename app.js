afb92e95aea590f5ae30369c4391e478d8c8478a
Mateusz <matyd356@gmail.com>
2021-02-01 12:46:31 +0100

    refactora infoSite using ExpressJS

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const router = require('./router')
 const port = 3000
 
 app.use(express.static('public'))
+
+
+app.get('*', function (req, res) {
+  return res.redirect('/404.html')
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 })
